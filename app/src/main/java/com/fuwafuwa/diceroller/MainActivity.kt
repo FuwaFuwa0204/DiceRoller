eb1943076894b4c3b57034171d1b2ae9b29d708a
FuwaFuwa0204 <soyeonmail24@gmail.com>
2023-04-17 22:54:12 +0900

    add wrapContentSize, Button composable

---
+++ app/src/main/java/com/fuwafuwa/diceroller/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.fuwafuwa.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.fuwafuwa.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier:Modifier = Modifier) {
+
+    Column(modifier=modifier,
+           horizontalAlignment = Alignment.CenterHorizontally){
+        Button(onClick = {}){
+            Text(stringResource(id = R.string.roll))
+        }
+
+    }
+
+}
